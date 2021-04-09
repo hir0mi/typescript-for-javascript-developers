fb4427ed2ce14ecebea0f63306f6be82fde9a121
Hiromi Nozawa <nozawa.hiromi@gmail.com>
2021-04-09 22:58:38 +0900

    add interfaces.ts!

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+};
+
+interface OjbectInterface {
+  name: string;
+  age: number;
+}
+
+let object: OjbectInterface = {
+  name: 'noza',
+  age: 86
+}
