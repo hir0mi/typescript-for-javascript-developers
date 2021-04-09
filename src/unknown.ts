35123b262d2292175b8e2af522a564e9b86a928a
Hiromi Nozawa <nozawa.hiromi@gmail.com>
2021-04-09 23:16:06 +0900

    add unknown.ts

---
+++ src/unknown.ts
@@ -0,0 +1,12 @@
+export {};
+
+const kansu = (): number => 43;
+
+let numberAny: any = kansu();
+let numberUnknown: unknown = kansu();
+
+let sumAny = numberAny + 10;
+if (typeof numberUnknown === 'number') {
+  let numUnknown = numberUnknown + 10;
+}
+
