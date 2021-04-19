4d00f97726b3def42f07a0da3b591a077adc7966
Hiromi Nozawa <nozawa.hiromi@gmail.com>
2021-04-19 21:15:18 +0900

    add type-assertion.ts

---
+++ src/type-assertion.ts
@@ -0,0 +1,11 @@
+export {};
+
+let name: any = 'noza';
+
+// let length = name.length as number;
+// let length = (name as string).length;
+let length = (<string>name).length;
+
+//length = 'foo';
+
+
